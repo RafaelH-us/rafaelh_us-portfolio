b5c372ce9c32f0db614adc66584a7225b9fccfef
Rafael Hernandez <rafaelh@rafaelh.us>
2020-10-21 09:24:42 -0700

    restructure site to SSR ReactJS ExpressJS Typescript

---
+++ server/public/precache-manifest.4b2faae005f4df5effd930a3e765e858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dbe3bddb64848862bd9481361ddd1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e67d5c667d33f625b9c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ad0cdc987b8d16c5eda",
+    "url": "/static/js/2.e08bf068.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e08bf068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e67d5c667d33f625b9c",
+    "url": "/static/js/main.0846d265.chunk.js"
+  },
+  {
+    "revision": "22cc05395d0d27bf7f8d",
+    "url": "/static/js/runtime-main.c234c952.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
